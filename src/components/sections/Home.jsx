3bd58dbcb2166648b7fa77d78e2ce683a9cc5c13
Abduboriy Ahmadjonov <ahmadjonovabduboriy3@gmail.com>
2025-07-15 03:24:44 +0500

    Changed home page

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -13,9 +13,8 @@ export const Home = () => {
           </h1>
 
           <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            I’m a full-stack developer who loves crafting clean, scalable web
-            applications. My goal is to build solutions that offer both
-            exceptional performance and a delightful user experience.
+            I’m a Full-Stack Developer with a backend focus. Also I am learning
+            how to build applications with AI (LangChain) in my free time.
           </p>
           <div className="flex justify-center space-x-4">
             <a
